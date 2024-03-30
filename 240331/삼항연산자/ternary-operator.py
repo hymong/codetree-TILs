14b52b665f056c178ac48213adac0dbfc747203f
hymong <165354828+hymong@users.noreply.github.com>
2024-03-31 00:06:39 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240331/삼항연산자/ternary-operator.py
+++ 240331/삼항연산자/ternary-operator.py
@@ -2,6 +2,6 @@ score = int(input())
 
 level= "0" if score == 100 else "1~9"
 
-print("pass") if int(level) == 0 else print("failure")
+print("pass" if int(level) == 0 else "failure")
 
 # '=' 대신에 '=='을 사용해야함!!!!
