26f1b3e3dd9bcd11352f604f808c167c54219fcb
hymong <165354828+hymong@users.noreply.github.com>
2024-04-02 20:59:55 +0900

    Title: 큰 수에서 빼기, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240402/큰 수에서 빼기/subtract-from-large-number.py
@@ -0,0 +1,21 @@
+# a = input()
+# arr = a.split()
+# if int(arr[0]) > int(arr[1]) : 
+#     print(int(arr[0]) - int(arr[1]))
+# else :
+#     print(int(arr[1]) - int(arr[0]))
+
+#map함수 쓰기
+a, b = map(int, input().split())
+
+if (a > b): print(a - b)
+else: print(b - a)
+
+#a = input()
+#arr = a.split()
+
+##for i in range(len(arr)):
+#     arr[i] = int(arr[i])
+
+# if (arr[0] > arr[1]): print(arr[0] - arr[1])
+# else: print(arr[1] - arr[0])
