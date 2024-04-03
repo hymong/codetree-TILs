3d57455bb303477d7ff05588db50eba13798899b
hymong <165354828+hymong@users.noreply.github.com>
2024-04-03 21:40:09 +0900

    Title: 등급 매기기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/등급 매기기/ranking.py
@@ -0,0 +1,11 @@
+level = int(input())
+if level >= 90:
+    print("A")
+elif level >= 80:
+    print("B")
+elif level >= 70:
+    print("c")
+elif level >= 60:
+    print("D")
+else :
+    print("F")
