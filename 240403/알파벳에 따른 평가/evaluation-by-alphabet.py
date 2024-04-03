639eb2ca5b4ea9f5051d4571e13fb4bd339b8451
hymong <165354828+hymong@users.noreply.github.com>
2024-04-03 21:45:38 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240403/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+capital = input()
+
+if capital == "S":
+    print("Superior")
+elif capital == "A":
+    print("Excellent")
+elif capital == "B":
+    print("Good")
+elif capital == "C":
+    print("Usually")
+elif calital == "D":
+    print("Effort")
+else :
+    print("Failure")
