2f6a70447f653f4d582d6254b7a92e90a7cd8a3c
hymong <165354828+hymong@users.noreply.github.com>
2024-04-04 17:26:04 +0900

    Title: 특정 조건 두 정수 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240404/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,16 @@
+input_value = input()
+a , b = map(int, input_value.split()) 
+import random
+
+#Python에서는 print 함수 사용시 기본적으로 한 줄이 띄어지게 됩니다. 출력 후 뒤에 
+#엔터가 아닌 공백을 출력하고 싶다면 print(값, end=" ") 형태로 작성해야 합니다.
+
+if a < b:
+    print("1",end = " ")
+else :
+    pritn("0",end = " ")
+
+if a == b:
+    print("1")
+else :
+    print("0")
