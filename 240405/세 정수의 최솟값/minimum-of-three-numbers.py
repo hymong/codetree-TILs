c7f3fd68622a2a91be72f9c10426903c09eaf000
hymong <165354828+hymong@users.noreply.github.com>
2024-04-05 15:48:03 +0900

    Title: 세 정수의 최솟값, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240405/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,9 +1,15 @@
 input_value = input()
 a, b, c = map(int, input_value.split())
 
-if a <= b and a <= c:
+if a < b and a < c:
     print(a)
-if b <= a and b <= c:
+if b < a and b < c:
     print(b)
-if c <= a and c <= b:
+if c < a and c < b:
     print(c)
+if a == b == c:
+    print(a)
+if a == b != c:
+    print(a)
+if a != b == c:
+    print(b)
