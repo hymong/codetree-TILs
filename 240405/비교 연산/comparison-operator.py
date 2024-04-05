72d28a502e5b4bee585384e2e5a4f625c3222d0d
hymong <165354828+hymong@users.noreply.github.com>
2024-04-05 14:36:22 +0900

    Title: 비교 연산, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240405/비교 연산/comparison-operator.py
@@ -0,0 +1,32 @@
+input_value = input()
+a , b = map(int, input_value.split())
+
+if a >= b :
+    print("1")
+else :
+    print("0")
+
+if a > b :
+    print("1")
+else :
+    print("0")
+
+if b >= a :
+    print("1")
+else :
+    print("0")
+
+if b > a :
+    print("1")
+else :
+    print("0")
+
+if a == b :
+    print("1")
+else :
+    print("0")
+
+if a != b :
+    print("1")
+else :
+    print("0")
