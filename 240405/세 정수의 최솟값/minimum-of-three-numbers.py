0309b92ba1d5834d05b317d92986e2efcb0b0428
hymong <165354828+hymong@users.noreply.github.com>
2024-04-05 15:52:23 +0900

    Title: 세 정수의 최솟값, Time: 169ms, Memory: 72MB, Status: Passed - CodeTree

--- 240405/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240405/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,9 +1,12 @@
 input_value = input()
 a, b, c = map(int, input_value.split())
 
+# a가 가장 작은 경우
 if a <= b and a <= c:
     print(a)
-if b <= a and b <= c:
+# a가 가장작지 않은 경우 중 b가 가장 작은 경우 
+elif b <= a and b <= c:
     print(b)
-if c <= a and c <= a:
+# a가 가장작지 않고 b도 가장 작지 않으므로 c가 가장 작은 경우
+else:
     print(c)
