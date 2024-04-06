41866ed86a13473072c9bb7d3377d8ea0772d1c9
hymong <165354828+hymong@users.noreply.github.com>
2024-04-06 17:27:22 +0900

    Title: 최댓값 구하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240406/최댓값 구하기/maximum-value.py
+++ 240406/최댓값 구하기/maximum-value.py
@@ -1,17 +1,9 @@
 input_value = input()
 a, b, c = map(int, input_value.split())
 
-if a > b and a > c:
+if a >= b and a >= c:
     print(a)
-if b > a and b > c:
+if b > a and b >= c:
     print(b)
 if c > a and c > b:
     print(c)
-if a == b == c:
-    print(a)
-elif a == b:
-    print(a)
-elif b == c:
-    print(b)
-elif a == c:
-    print(a)
