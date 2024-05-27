511dec811ab79900b22e3c5dadbf4add4682a18f
hymong <165354828+hymong@users.noreply.github.com>
2024-05-27 17:59:21 +0900

    Title: ab 사이에 없는 c, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240527/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240527/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,11 +1,11 @@
 a, b, c = map(int, input().split())
-result = "False"
+result = False
 
 for i in range(a, b+1):
     if i%c == 0:
-        result == "True"
+        result == True
 
-if result == "True":
+if result == True :
     print("NO")
 else:
     print("YES")
