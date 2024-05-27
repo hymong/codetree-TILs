02588a27f6acca66a4407a798190a1247fbad748
hymong <165354828+hymong@users.noreply.github.com>
2024-05-27 18:07:39 +0900

    Title: 모두 3의 배수, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240527/모두 3의 배수/all-multiples-of-3.py
@@ -0,0 +1,13 @@
+result = False
+
+for i in range(5):
+    n = int(input())
+    if n%3 == 0:
+        result = True
+    else:
+        result = False
+
+if result == True:
+    print(1)
+else:
+    print(0)
